9f8c0df04818ad94b6e98516a800fd8d55d22e27
David Feather <davidjfeather@gmail.com>
2022-04-03 15:46:24 +0100

    update todo list

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 """ this will be the thing that is run
 
+TODO
+- a smarter ai - maybe recursion to decide on a move?
+
+- write some tests for the methods
+
+- add ability to set names
+
+- add functionality to set the starting player, chosen randomly, or alternatiing
+
 
 
 """
-import board
+
 import game
 
 if __name__ == '__main__' :
